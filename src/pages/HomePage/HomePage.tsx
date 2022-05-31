71934bde9db1b4ef2e21bfc068de90d6dad39c80
Gerard Albajar <gerard.albajar@gmail.com>
2022-05-31 09:57:03 +0200

    Adding and typing HomePage.tsx component

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,26 @@
+import HomePageStyled from "./HomePageStyled";
+
+const HomePage = (): JSX.Element => {
+  return (
+    <HomePageStyled>
+      <header>
+        <img src="images/logo.png" alt="" />
+      </header>
+      <h2> CHECK-IN </h2>
+      <p>
+        The entirety of humankind's existence has led to this moment. We have
+        set foot on the Martian surface. Astro Factory - Mars Rock NFT Mining
+        Mission is live. RESTRICTED ACCESS - Mothership Ticket NFT required.
+        Mission ends Jun 17th, 2022.
+      </p>
+      <p>We boldly go where no Mining Mission has gone before. Join us.</p>
+      <p>Select Mining Experience:</p>
+      <div>
+        <button>Log-In</button>
+        <button>Sign-Up</button>
+      </div>
+    </HomePageStyled>
+  );
+};
+
+export default HomePage;
