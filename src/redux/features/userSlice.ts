aabfd11209a47e9c120312d778a9e44eda3648da
Gerard Albajar <gerard.albajar@gmail.com>
2022-05-31 13:58:58 +0200

    Adding userSlice and store with it's types

---
+++ src/redux/features/userSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User, UserInfo } from "../../types/types";
+
+const initialState: User = {
+  name: "",
+  username: "",
+  logged: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+
+  reducers: {
+    login: (user: User, action: PayloadAction<UserInfo>) => ({
+      ...action.payload,
+      logged: true,
+    }),
+    logout: () => ({ name: "", username: "", logged: false }),
+  },
+});
+
+export const { login: loginActionCreator, logout: logoutActionCreator } =
+  userSlice.actions;
+
+export default userSlice.reducer;
