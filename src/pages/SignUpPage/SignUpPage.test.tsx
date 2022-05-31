f0b8ae098a16538b3017349520f10aecc0025927
Gerard Albajar <gerard.albajar@gmail.com>
2022-05-31 12:07:51 +0200

    Refactorizing HomePage, LogInPage and SignUp tests

--- src/pages/SignUpPage/SignUpPage.test.tsx
+++ src/pages/SignUpPage/SignUpPage.test.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
 import SignUpPage from "./SignUpPage";
 const { render, screen } = require("@testing-library/react");
 
 describe("Given a SignUpPage Component", () => {
   describe("When it's invoked)", () => {
     test("Then it should render a the SignUpPage component with two buttons elements", () => {
-      render(<SignUpPage />);
+      render(
+        <BrowserRouter>
+          <SignUpPage />
+        </BrowserRouter>
+      );
 
       const expectedNumberOfButtons = 3;
 
@@ -14,25 +19,41 @@ describe("Given a SignUpPage Component", () => {
     });
 
     test("Then it should render one 'Username' label", () => {
-      render(<SignUpPage />);
+      render(
+        <BrowserRouter>
+          <SignUpPage />
+        </BrowserRouter>
+      );
 
       expect(screen.getAllByText("Username")).toHaveLength(1);
     });
 
     test("Then it should render one 'Password' label", () => {
-      render(<SignUpPage />);
+      render(
+        <BrowserRouter>
+          <SignUpPage />
+        </BrowserRouter>
+      );
 
       expect(screen.getAllByText("Password")).toHaveLength(1);
     });
 
     test("Then it should render one 'Mail' label", () => {
-      render(<SignUpPage />);
+      render(
+        <BrowserRouter>
+          <SignUpPage />
+        </BrowserRouter>
+      );
 
       expect(screen.getAllByText("Mail")).toHaveLength(1);
     });
 
     test("Then it should render one 'Name' label", () => {
-      render(<SignUpPage />);
+      render(
+        <BrowserRouter>
+          <SignUpPage />
+        </BrowserRouter>
+      );
 
       expect(screen.getAllByText("Name")).toHaveLength(1);
     });
