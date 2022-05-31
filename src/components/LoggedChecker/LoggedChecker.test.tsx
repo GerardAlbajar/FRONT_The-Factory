e6a83172ceeff919cc6a923fb3cfb6c5886d9f17
Gerard Albajar <gerard.albajar@gmail.com>
2022-05-31 23:25:59 +0200

    Testing LoggedChecker component if it returns its children

---
+++ src/components/LoggedChecker/LoggedChecker.test.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from "@testing-library/react";
+import LoggedChecker from "./LoggedChecker";
+
+const mockUseNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-redux"),
+  useNavigate: () => mockUseNavigate,
+}));
+
+let mockLogged = false;
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useSelector: () => ({ logged: mockLogged }),
+}));
+
+describe("Given a LoggedChecked function", () => {
+  describe("When it's invoked", () => {
+    test("Then it should navigarte to login enpoint when user is not logged", () => {
+      render(
+        <LoggedChecker>
+          <h1>Hello</h1>
+        </LoggedChecker>
+      );
+
+      expect(mockUseNavigate).toHaveBeenCalledWith("/login");
+    });
+
+    test("Then it should render its children when the user is logged", () => {
+      mockLogged = true;
+
+      const expectedHeaders = 1;
+
+      render(
+        <LoggedChecker>
+          <h1>Hello</h1>
+        </LoggedChecker>
+      );
+
+      const searchedText = screen.getAllByRole("heading");
+
+      expect(searchedText).toHaveLength(expectedHeaders);
+    });
+  });
+});
