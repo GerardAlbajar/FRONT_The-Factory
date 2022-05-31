a43fd8ace7658555a75c3417b66e9b3fa466267e
Gerard Albajar <gerard.albajar@gmail.com>
2022-05-31 22:20:24 +0200

    Refactorizing LogInForm.tsx and its tests

--- src/components/LogInForm/LogInForm.test.tsx
+++ src/components/LogInForm/LogInForm.test.tsx
@@ -2,14 +2,14 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import store from "../../redux/store/store";
-import Login from "./LogInForm";
+import LogInForm from "./LogInForm";
 
 describe("Given the LogInForm component", () => {
   describe("When it's invoked", () => {
     test("Then it should render one 'Username' label", () => {
       render(
         <Provider store={store}>
-          <Login />
+          <LogInForm />
         </Provider>
       );
 
@@ -19,7 +19,7 @@ describe("Given the LogInForm component", () => {
     test("Then it should render one 'Password' label", () => {
       render(
         <Provider store={store}>
-          <Login />
+          <LogInForm />
         </Provider>
       );
 
@@ -29,7 +29,7 @@ describe("Given the LogInForm component", () => {
     test("Then it should render a button", () => {
       render(
         <Provider store={store}>
-          <Login />
+          <LogInForm />
         </Provider>
       );
 
@@ -43,7 +43,7 @@ describe("Given the LogInForm component", () => {
 
       render(
         <Provider store={store}>
-          <Login />
+          <LogInForm />
         </Provider>
       );
 
@@ -56,12 +56,34 @@ describe("Given the LogInForm component", () => {
 
       render(
         <Provider store={store}>
-          <Login />
+          <LogInForm />
         </Provider>
       );
 
       userEvent.type(screen.getByLabelText("Password"), inputText);
       expect(screen.getByLabelText("Password")).toHaveValue(inputText);
+    });
+  });
+
+  describe("When the user type whatever the username and password input fields", () => {
+    test("Then the Log-In button should be enabled", () => {
+      const username = "test";
+      const password = "test";
+
+      render(
+        <Provider store={store}>
+          <LogInForm />
+        </Provider>
+      );
+
+      const usernameInput = screen.getByLabelText("Username");
+      const passwordInput = screen.getByLabelText("Password");
+      const loginButton = screen.getByRole("button", { name: "Log-In" });
+
+      userEvent.type(usernameInput, username);
+      userEvent.type(passwordInput, password);
+
+      expect(loginButton).not.toBeDisabled();
     });
   });
 });
