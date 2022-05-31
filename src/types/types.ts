db41ac8435e6c7be9460fab0593348937451a711
Gerard Albajar <gerard.albajar@gmail.com>
2022-05-31 22:42:06 +0200

    Adding Part, Astro and Item interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -20,3 +20,21 @@ export interface UserRegister {
   username: string;
   password: string;
 }
+
+export interface Part {
+  id: string;
+  type: "rocket" | "astro" | "naut";
+  framework: "react" | "angular" | "vue" | "jquery";
+  assembled: boolean;
+}
+
+export interface Astro {
+  id: string;
+  perfect: boolean;
+  parts: Part[];
+}
+
+export interface Item {
+  type: "part" | "set";
+  data: Part | Astro;
+}
