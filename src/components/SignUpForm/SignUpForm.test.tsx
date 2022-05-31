6fc283bdf082bf48ddd2d43eb4381870a3db322c
Gerard Albajar <gerard.albajar@gmail.com>
2022-05-31 16:15:45 +0200

    Adding Provider at all related SignUpForm tests

--- src/components/SignUpForm/SignUpForm.test.tsx
+++ src/components/SignUpForm/SignUpForm.test.tsx
@@ -1,34 +1,56 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import store from "../../redux/store/store";
 import SignUpForm from "./SignUpForm";
 
 describe("Given the SignUpForm component", () => {
   describe("When it's invoked", () => {
     test("Then it should render one 'Name' label", () => {
-      render(<SignUpForm />);
+      render(
+        <Provider store={store}>
+          <SignUpForm />
+        </Provider>
+      );
 
       expect(screen.getAllByText("Name")).toHaveLength(1);
     });
 
     test("Then it should render one 'Mail' label", () => {
-      render(<SignUpForm />);
+      render(
+        <Provider store={store}>
+          <SignUpForm />
+        </Provider>
+      );
 
       expect(screen.getAllByText("Mail")).toHaveLength(1);
     });
 
     test("Then it should render one 'Username' label", () => {
-      render(<SignUpForm />);
+      render(
+        <Provider store={store}>
+          <SignUpForm />
+        </Provider>
+      );
 
       expect(screen.getAllByText("Username")).toHaveLength(1);
     });
 
     test("Then it should render one 'Password' label", () => {
-      render(<SignUpForm />);
+      render(
+        <Provider store={store}>
+          <SignUpForm />
+        </Provider>
+      );
 
       expect(screen.getAllByText("Password")).toHaveLength(1);
     });
 
     test("Then it should render a button", () => {
-      render(<SignUpForm />);
+      render(
+        <Provider store={store}>
+          <SignUpForm />
+        </Provider>
+      );
 
       expect(screen.getByRole("button")).toBeInTheDocument();
     });
--- src/pages/SignUpPage/SignUpPage.test.tsx
+++ src/pages/SignUpPage/SignUpPage.test.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "../../redux/store/store";
 import SignUpPage from "./SignUpPage";
 const { render, screen } = require("@testing-library/react");
 
@@ -6,9 +8,11 @@ describe("Given a SignUpPage Component", () => {
   describe("When it's invoked)", () => {
     test("Then it should render a the SignUpPage component with two buttons elements", () => {
       render(
-        <BrowserRouter>
-          <SignUpPage />
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <SignUpPage />
+          </BrowserRouter>
+        </Provider>
       );
 
       const expectedNumberOfButtons = 3;
@@ -20,9 +24,11 @@ describe("Given a SignUpPage Component", () => {
 
     test("Then it should render one 'Username' label", () => {
       render(
-        <BrowserRouter>
-          <SignUpPage />
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <SignUpPage />
+          </BrowserRouter>
+        </Provider>
       );
 
       expect(screen.getAllByText("Username")).toHaveLength(1);
@@ -30,9 +36,11 @@ describe("Given a SignUpPage Component", () => {
 
     test("Then it should render one 'Password' label", () => {
       render(
-        <BrowserRouter>
-          <SignUpPage />
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <SignUpPage />
+          </BrowserRouter>
+        </Provider>
       );
 
       expect(screen.getAllByText("Password")).toHaveLength(1);
@@ -40,9 +48,11 @@ describe("Given a SignUpPage Component", () => {
 
     test("Then it should render one 'Mail' label", () => {
       render(
-        <BrowserRouter>
-          <SignUpPage />
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <SignUpPage />
+          </BrowserRouter>
+        </Provider>
       );
 
       expect(screen.getAllByText("Mail")).toHaveLength(1);
@@ -50,9 +60,11 @@ describe("Given a SignUpPage Component", () => {
 
     test("Then it should render one 'Name' label", () => {
       render(
-        <BrowserRouter>
-          <SignUpPage />
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <SignUpPage />
+          </BrowserRouter>
+        </Provider>
       );
 
       expect(screen.getAllByText("Name")).toHaveLength(1);
