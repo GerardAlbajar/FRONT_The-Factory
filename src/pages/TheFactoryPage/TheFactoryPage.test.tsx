0c758053f33dbca4c736a6637b755348ef17a283
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-02 16:09:08 +0200

    Testing TheFactoryPage.tsx at pages/TheFactoryPage

---
+++ src/pages/TheFactoryPage/TheFactoryPage.test.tsx
@@ -0,0 +1,47 @@
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "../../redux/store/store";
+import TheFactoryPage from "./TheFactoryPage";
+const { render, screen } = require("@testing-library/react");
+
+describe("Given a HomePage Component", () => {
+  describe("When it's invoked)", () => {
+    test("Then it should render a the HomePage component with two buttons elements", () => {
+      render(
+        <BrowserRouter>
+          <TheFactoryPage />
+        </BrowserRouter>
+      );
+
+      const expectedNumberOfButtons = 1;
+
+      const totalList = screen.getAllByRole("button");
+
+      expect(totalList.length).toBe(expectedNumberOfButtons);
+    });
+
+    test("Then it should render one 'THE FACTORY' heading", () => {
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <TheFactoryPage />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      expect(screen.getAllByText("THE FACTORY")).toHaveLength(1);
+    });
+
+    test("Then it should render four number of headings", () => {
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <TheFactoryPage />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      expect(screen.getAllByRole("heading")).toHaveLength(4);
+    });
+  });
+});
