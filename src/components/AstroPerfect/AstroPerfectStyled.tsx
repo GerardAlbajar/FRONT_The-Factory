f9378db5363ec487fbb895ed97a4ec067dbb65b5
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-03 00:28:50 +0200

    Adding AstroPerfectStyled.tsx at components/AstroPerfect

---
+++ src/components/AstroPerfect/AstroPerfectStyled.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const AstroPerfectStyled = styled.li`
+  .astro {
+    display: flex;
+    position: relative;
+    left: 0;
+
+    img {
+      width: 200px;
+      position: absolute;
+      left: 0;
+      padding-bottom: 50px;
+    }
+  }
+`;
+
+export default AstroPerfectStyled;
