63e4d278760654b5c77b30c233e9d415d6c131d7
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-02 12:05:45 +0200

    Updating userThunks with errors and success messages

--- src/redux/thunks/userThunks/userThunks.tsx
+++ src/redux/thunks/userThunks/userThunks.tsx
@@ -1,40 +1,54 @@
 import axios from "axios";
 import jwtDecode from "jwt-decode";
+import toast from "react-hot-toast";
 import { UserInfo, UserLogin, UserRegister } from "../../../types/types";
 import { logInActionCreator } from "../../features/userSlice";
 import { AppDispatch } from "../../store/store";
 
 export const logInUserThunk =
   (loginInformation: UserLogin) => async (dispatch: AppDispatch) => {
-    const route: string = `${process.env.REACT_APP_API_URL}user/login`;
-    const {
-      data: { token },
-    } = await axios.post(route, loginInformation);
+    try {
+      const route: string = `${process.env.REACT_APP_API_URL}user/login`;
+      const {
+        data: { token },
+      } = await axios.post(route, loginInformation);
 
-    localStorage.setItem("TokenKey", token);
+      localStorage.setItem("TokenKey", token);
 
-    const userInfo: UserInfo = jwtDecode(token);
+      const userInfo: UserInfo = jwtDecode(token);
 
-    dispatch(logInActionCreator(userInfo));
+      dispatch(logInActionCreator(userInfo));
+      toast.success("Log-In successfull");
+    } catch (error) {
+      toast.error("Wrong Username or Password");
+    }
   };
 
 export const signUpUserThunk =
   (formData: UserRegister) => async (dispatch: AppDispatch) => {
-    await axios.post(`${process.env.REACT_APP_API_URL}user/register`, formData);
+    try {
+      await axios.post(
+        `${process.env.REACT_APP_API_URL}user/register`,
+        formData
+      );
 
-    const newUser = {
-      username: formData.username,
-      password: formData.password,
-    };
+      const newUser = {
+        username: formData.username,
+        password: formData.password,
+      };
 
-    const route = `${process.env.REACT_APP_API_URL}user/login`;
-    const {
-      data: { token },
-    } = await axios.post(route, newUser);
+      const route = `${process.env.REACT_APP_API_URL}user/login`;
+      const {
+        data: { token },
+      } = await axios.post(route, newUser);
 
-    localStorage.setItem("TokenKey", token);
+      localStorage.setItem("TokenKey", token);
 
-    const userInfo: UserInfo = jwtDecode(token);
+      const userInfo: UserInfo = jwtDecode(token);
 
-    dispatch(logInActionCreator(userInfo));
+      dispatch(logInActionCreator(userInfo));
+      toast.success("Sign-Up successfull");
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
   };
