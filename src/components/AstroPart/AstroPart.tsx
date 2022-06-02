d0c4ada4beeb82aa507ec8f7af748895ce6f00cb
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-03 00:27:35 +0200

    Adding AstroPrt.tsx at components/AstroPart

---
+++ src/components/AstroPart/AstroPart.tsx
@@ -0,0 +1,18 @@
+import { AstroPartProps } from "../../types/types";
+import AstroPartStyled from "./AstroPartStyled";
+
+const AstroPart = ({ name, image, id }: AstroPartProps): JSX.Element => {
+  return (
+    <AstroPartStyled>
+      <ul>
+        <li>
+          <img src={image} alt={name} />
+        </li>
+        <li>{name}</li>
+        <li>Id: {id}</li>
+      </ul>
+    </AstroPartStyled>
+  );
+};
+
+export default AstroPart;
