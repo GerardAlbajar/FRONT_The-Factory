0641e931e4c0404f6f6a16ba84412bcc4f4d2d84
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-03 00:34:22 +0200

    Adding mockAstros and mockAstroParts because of tests

---
+++ src/mocks/astroParts.js
@@ -0,0 +1,30 @@
+const mockAstroParts = [
+  {
+    id: "#812736SJDGHA",
+    name: "REACT ASTRO",
+    type: "Astro",
+    framework: "React",
+    assembled: "false",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/astro-factory.appspot.com/o/REACT_ASTRO.png?alt=media&token=8934e3f3-8e42-4ddd-8dec-9fe74acc6c47",
+    partimage:
+      "https://firebasestorage.googleapis.com/v0/b/astro-factory.appspot.com/o/REACT_ASTRO_2.png?alt=media&token=7e1ef3af-b744-4d81-9c44-20346c04977e",
+    owner: "6294eeb64326e4549274b515",
+  },
+  {
+    id: "#812736SJDGHB",
+    name: "REACT NAUT",
+    type: "Naut",
+    framework: "React",
+    assembled: "false",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/astro-factory.appspot.com/o/REACT_NAUT.png?alt=media&token=b4ca79a2-7a99-4674-93d2-78a2bba4322f",
+    partimage:
+      "https://firebasestorage.googleapis.com/v0/b/astro-factory.appspot.com/o/REACT_NAUT_2.png?alt=media&token=b7217d8c-f46c-422b-a2a4-827f89116a00",
+    owner: "6294eeb64326e4549274b515",
+  },
+];
+
+module.exports = {
+  mockAstroParts,
+};
