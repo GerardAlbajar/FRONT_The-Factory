88e16ebc3dc6d31229b6ed8902423eabd6eea37d
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-03 10:01:52 +0200

    Refactorizing AstroPerfect.tsx and adding AstroPerfectStyled.tsx

--- src/components/AstroPerfect/AstroPerfectStyled.tsx
+++ src/components/AstroPerfect/AstroPerfectStyled.tsx
@@ -1,17 +1,34 @@
 import styled from "styled-components";
 
 const AstroPerfectStyled = styled.li`
+  li {
+    list-style: none;
+
+    text-align: left;
+
+    p {
+      font-weight: bolder;
+      margin: 0;
+    }
+  }
+
   .astro {
     display: flex;
     position: relative;
     left: 0;
+    height: 363px;
 
     img {
       width: 200px;
       position: absolute;
       left: 0;
-      padding-bottom: 50px;
+      padding: 50px 0 20px;
     }
+  }
+
+  ul {
+    width: 200px;
+    padding: 0 30px;
   }
 `;
 
