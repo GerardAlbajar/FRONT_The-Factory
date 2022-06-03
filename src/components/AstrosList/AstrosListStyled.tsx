97848302f5fff513d8f2f6b1f39f5b8339e2b3e9
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-03 10:02:32 +0200

    Refactorizing AstrosList.tsx and adding AstrosListStyled.tsx

---
+++ src/components/AstrosList/AstrosListStyled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const AstrosListStyled = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  padding-left: 0;
+`;
+
+export default AstrosListStyled;
