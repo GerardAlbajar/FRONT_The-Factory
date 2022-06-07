c05c84fe1452a1cc8f7aadbf39a3d37ba49643bd
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-07 13:27:40 +0200

    Updating Header component changing alt image

--- src/components/HeaderComponent/Header.test.tsx
+++ src/components/HeaderComponent/Header.test.tsx
@@ -1,8 +1,16 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "../../redux/store/store";
 import Header from "./Header";
+
+const mockDispatch = jest.fn();
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
+}));
 
 describe("Given a Header Component", () => {
   describe("When its called to be rendered", () => {
@@ -17,6 +25,27 @@ describe("Given a Header Component", () => {
 
       const displayHeader = screen.getAllByRole("listitem");
       expect(displayHeader.length).toBe(5);
+    });
+  });
+});
+
+describe("Given the AstroPart Component", () => {
+  describe("When a user enters its username and password and submit the form", () => {
+    test("Then it should call the dispatch action with its credentials", () => {
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <Header />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const expectedClick: HTMLButtonElement = screen.getByRole("link", {
+        name: "Log Out",
+      });
+      userEvent.click(expectedClick);
+
+      expect(mockDispatch).toHaveBeenCalled();
     });
   });
 });
