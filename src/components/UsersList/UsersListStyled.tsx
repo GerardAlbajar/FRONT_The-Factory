ee9e9455ee446f731cb84a6a73b612eb0e0dfc81
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-14 11:54:35 +0200

    Adding UsersList.tsx with its test and Styled component

---
+++ src/components/UsersList/UsersListStyled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const UsersListStyled = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  padding-left: 0;
+`;
+
+export default UsersListStyled;
